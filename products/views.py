928c1605ddaba5c8d0c8e627701ac3e8d4b51e5a
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-09-23 18:18:42 +0000

    produect details page added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -17,10 +17,10 @@ def all_products(request):
 # A view to return a single product
 def product_detail(request, product_id):
 
-    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
